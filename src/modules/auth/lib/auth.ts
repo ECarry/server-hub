10987da7c3ebd2086ac6202fffacff65717b3d7f
ecarry <lianshiliang93@gmail.com>
2025-08-14 15:44:07 +0000

    feat: implement authentication with better-auth library

---
+++ src/modules/auth/lib/auth.ts
@@ -0,0 +1,37 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+import { admin } from "better-auth/plugins";
+import { passkey } from "better-auth/plugins/passkey";
+import { cache } from "react";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...schema,
+    },
+  }),
+  // user: {
+  //   additionalFields: {
+  //     role: { type: "string" },
+  //   },
+  // },
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [admin(), passkey()],
+});
+
+export const getSession = cache(auth.api.getSession);
