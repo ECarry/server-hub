709affb0ff73a385c4f91a5ff18fc985d61ed281
ecarry <lianshiliang93@gmail.com>
2025-08-16 15:16:05 +0000

    feat: implement auth UI with sign-in and sign-up pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/modules/auth/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
+
+  return <SignInView />;
+};
+
+export default page;
