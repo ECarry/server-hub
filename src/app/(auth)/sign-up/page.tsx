296bf499204e807ca7b4ab859d8ef165cb5fb19a
ecarry <lianshiliang93@gmail.com>
2025-12-01 01:04:04 +0800

    feat: Implement sign-in and sign-up pages with email/password and social authentication.

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/modules/auth/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
+
+  return <SignUpView />;
+};
+
+export default page;
