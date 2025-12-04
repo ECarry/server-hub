9a1399e4900233252aa3d8d056eed9d746d277c5
ecarry <lianshiliang93@gmail.com>
2025-12-04 19:23:49 +0800

    feat: implement empty state and skeleton loader improvements for Brands and Series views

--- src/modules/series/ui/views/series-view.tsx
+++ src/modules/series/ui/views/series-view.tsx
@@ -15,6 +15,17 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { useSeriesFilters } from "../../hooks/use-series-filters";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { IconPhotoOff } from "@tabler/icons-react";
+import { Activity } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export const SeriesView = () => {
   const [filters, setFilters] = useSeriesFilters();
@@ -27,82 +38,75 @@ export const SeriesView = () => {
   );
 
   return (
-    <div className="flex flex-1 flex-col">
-      {/* <BrandCreateModal open={open} onOpenChange={setOpen} /> */}
-      <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <div className="w-full flex-col justify-start space-y-6">
-            {/* <BrandCreateModal open={isOpen} onOpenChange={setIsOpen} /> */}
-            <div className="flex items-center px-4 lg:px-6">
-              {/* <h1 className="text-2xl font-bold">Series</h1> */}
-              <div className="flex ml-auto items-center gap-2">
-                {/* <SeriesSearchFilter /> */}
-                {/* <Button
-                  variant="outline"
-                  size="sm"
-                  onClick={() => setIsOpen(true)}
-                >
-                  <IconPlus />
-                  <span className="hidden lg:inline">Add Brand</span>
-                </Button> */}
-              </div>
-            </div>
-            <div className="relative flex flex-col gap-4 overflow-auto px-4 lg:px-6 mt-4">
-              <div className="overflow-hidden rounded-lg">
-                <DataTable columns={columns} data={data.items} />
-              </div>
-              <DataPagination
-                page={filters.page}
-                totalPages={data.totalPages}
-                onPageChange={(page) => {
-                  setFilters({ page });
-                }}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="px-4 md:px-8">
+      <Activity mode={data.total === 0 ? "hidden" : "visible"}>
+        <DataTable columns={columns} data={data.items} />
+        <DataPagination
+          page={filters.page}
+          totalPages={data.totalPages}
+          onPageChange={(page) => {
+            setFilters({ page });
+          }}
+        />
+      </Activity>
+
+      <Activity mode={data.total !== 0 ? "hidden" : "visible"}>
+        <EmptyStatus />
+      </Activity>
     </div>
+  );
+};
+
+const EmptyStatus = () => {
+  return (
+    <Empty className="border border-dashed">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <IconPhotoOff />
+        </EmptyMedia>
+        <EmptyTitle>No series found</EmptyTitle>
+        <EmptyDescription>
+          You have no series. Create a new series to get started.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent></EmptyContent>
+    </Empty>
   );
 };
 
 export const SeriesViewSkeleton = () => {
   return (
-    <div className="flex flex-1 flex-col">
-      <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <div className="w-full flex-col justify-start space-y-6">
-            <div className="flex items-center px-4 lg:px-6">
-              <h1 className="text-2xl font-bold">Brands</h1>
-              <div className="flex ml-auto items-center gap-2"></div>
-            </div>
-            <div className="relative flex flex-col gap-4 overflow-auto px-4 lg:px-6 mt-4">
-              <div className="overflow-hidden rounded-lg border">
-                <Table>
-                  <TableHeader className="bg-muted sticky top-0 z-10">
-                    <TableRow>
-                      {Array.from({ length: 5 }).map((_, index) => (
-                        <TableHead key={index}>
-                          <Skeleton className="h-8 w-24" />
-                        </TableHead>
-                      ))}
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody className="**:data-[slot=table-cell]:first:w-8">
-                    {Array.from({ length: 10 }).map((_, index) => (
-                      <TableRow key={index}>
-                        {Array.from({ length: 5 }).map((_, cellIndex) => (
-                          <TableCell key={cellIndex}>
-                            <Skeleton className="h-8 w-24" />
-                          </TableCell>
-                        ))}
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
-            </div>
-          </div>
+    <div className="px-4 md:px-8 space-y-4">
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader className="sticky top-0 z-10 bg-muted">
+            <TableRow>
+              <TableHead><Checkbox /></TableHead>
+              <TableHead>Logo</TableHead>
+              <TableHead>Name</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="**:data-[slot=table-cell]:first:w-8">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
+                {Array.from({ length: 2 }).map((_, cellIndex) => (
+                  <TableCell key={cellIndex}>
+                    <Skeleton className="h-8 w-24" />
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-between">
+        <Skeleton className="h-4 w-20" />
+        <div className="flex items-center gap-x-2">
+          <Skeleton className="h-4 w-20" />
+          <Skeleton className="h-4 w-20" />
         </div>
       </div>
     </div>
